6ed5e7357601ee24473a69212c7d23f2c51aa83c
Oliver Lambson <oliverlambson@gmail.com>
2021-01-01 14:09:09 +0100

    added netlify cms

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,9 +25,10 @@ const Layout = ({ location, title, children }) => {
       <header className="global-header">{header}</header>
       <main>{children}</main>
       <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
+        © Oliver Lambson {new Date().getFullYear()}
+        <br />
+        Built with ♥ using <a href="https://www.gatsbyjs.com">Gatsby</a>,{" "}
+        <a href="https://www.netlify.com/">Netlify</a> and other OSS.
       </footer>
     </div>
   )
